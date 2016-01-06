db61931f842e46bc021ae72436afa3da2c6529f2
Manish Marahatta <manishmarahatta1996@gmail.com>
2016-01-06 10:20:09 +0545

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>websearch</title>
+    </head>
+    <body>
+    <div align="center">
+<form action="search.php" method="post">  
+Search: <input type="text" name="search" placeholder="search" />  
+<input type="submit" value="submit" />  
+</form>  
+</div>
+ </body>
+</html>
